c6964232f37a1ca2ff849d487bacb6b376c8d22d
hys909 <hys909@gmail.com>
2026-06-13 22:29:01 +0800

    Expand currency list to 46 entries

--- src/constants/currencies.js
+++ src/constants/currencies.js
@@ -3,11 +3,16 @@ export const currencies = [
     { code: 'USD', name: '美元', flag: 'us' }, { code: 'EUR', name: '歐元', flag: 'eu' }, { code: 'JPY', name: '日圓', flag: 'jp' },
     { code: 'GBP', name: '英鎊', flag: 'gb' }, { code: 'AUD', name: '澳幣', flag: 'au' }, { code: 'CAD', name: '加幣', flag: 'ca' },
     { code: 'CHF', name: '瑞士法郎', flag: 'ch' }, { code: 'CNY', name: '人民幣', flag: 'cn' }, { code: 'HKD', name: '港幣', flag: 'hk' },
-    { code: 'KRW', name: '韓圓', flag: 'kr' }, { code: 'SGD', name: '新加坡幣', flag: 'sg' },
-    { code: 'NZD', name: '紐西蘭幣', flag: 'nz' }, { code: 'THB', name: '泰銖', flag: 'th' }, { code: 'INR', name: '印度盧比', flag: 'in' },
-    { code: 'BRL', name: '巴西雷亞爾', flag: 'br' }, { code: 'RUB', name: '俄羅斯盧布', flag: 'ru' }, { code: 'MXN', name: '墨西哥披索', flag: 'mx' },
-    { code: 'IDR', name: '印尼盾', flag: 'id' }, { code: 'TRY', name: '土耳其里拉', flag: 'tr' }, { code: 'SAR', name: '沙烏地里亞爾', flag: 'sa' },
-    { code: 'SEK', name: '瑞典克朗', flag: 'se' }, { code: 'NOK', name: '挪威克朗', flag: 'no' }, { code: 'DKK', name: '丹麥克朗', flag: 'dk' },
-    { code: 'PLN', name: '波蘭茲羅提', flag: 'pl' }, { code: 'ZAR', name: '南非蘭特', flag: 'za' }, { code: 'MYR', name: '馬來西亞令吉', flag: 'my' },
-    { code: 'PHP', name: '菲律賓披索', flag: 'ph' }, { code: 'VND', name: '越南盾', flag: 'vn' }, { code: 'AED', name: '阿聯酋迪拉姆', flag: 'ae' }
+    { code: 'KRW', name: '韓圓', flag: 'kr' }, { code: 'SGD', name: '新加坡幣', flag: 'sg' }, { code: 'NZD', name: '紐西蘭幣', flag: 'nz' },
+    { code: 'THB', name: '泰銖', flag: 'th' }, { code: 'INR', name: '印度盧比', flag: 'in' }, { code: 'BRL', name: '巴西雷亞爾', flag: 'br' },
+    { code: 'RUB', name: '俄羅斯盧布', flag: 'ru' }, { code: 'MXN', name: '墨西哥披索', flag: 'mx' }, { code: 'IDR', name: '印尼盾', flag: 'id' },
+    { code: 'TRY', name: '土耳其里拉', flag: 'tr' }, { code: 'SAR', name: '沙烏地里亞爾', flag: 'sa' }, { code: 'SEK', name: '瑞典克朗', flag: 'se' },
+    { code: 'NOK', name: '挪威克朗', flag: 'no' }, { code: 'DKK', name: '丹麥克朗', flag: 'dk' }, { code: 'PLN', name: '波蘭茲羅提', flag: 'pl' },
+    { code: 'ZAR', name: '南非蘭特', flag: 'za' }, { code: 'MYR', name: '馬來西亞令吉', flag: 'my' }, { code: 'PHP', name: '菲律賓披索', flag: 'ph' },
+    { code: 'VND', name: '越南盾', flag: 'vn' }, { code: 'AED', name: '阿聯酋迪拉姆', flag: 'ae' }, { code: 'QAR', name: '卡達里亞爾', flag: 'qa' },
+    { code: 'EGP', name: '埃及鎊', flag: 'eg' }, { code: 'CLP', name: '智利披索', flag: 'cl' }, { code: 'COP', name: '哥倫比亞披索', flag: 'co' },
+    { code: 'ARS', name: '阿根廷披索', flag: 'ar' }, { code: 'HUF', name: '匈牙利福林', flag: 'hu' }, { code: 'CZK', name: '捷克克朗', flag: 'cz' },
+    { code: 'ILS', name: '以色列謝克爾', flag: 'il' }, { code: 'RON', name: '羅馬尼亞列伊', flag: 'ro' }, { code: 'NGN', name: '奈及利亞奈拉', flag: 'ng' },
+    { code: 'PKR', name: '巴基斯坦盧比', flag: 'pk' }, { code: 'KWD', name: '科威特第納爾', flag: 'kw' }, { code: 'BHD', name: '巴林第納爾', flag: 'bh' },
+    { code: 'OMR', name: '阿曼里亞爾', flag: 'om' }, { code: 'JOD', name: '約旦第納爾', flag: 'jo' }, { code: 'LKR', name: '斯里蘭卡盧比', flag: 'lk' }
 ];
